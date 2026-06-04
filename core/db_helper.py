cef054e722b8af48ee786c0ffc73a20faddc9549
redun <redunsasha2017@gmail.com>
2026-06-04 13:25:10 +0700

    add .env

---
+++ core/db_helper.py
@@ -0,0 +1,37 @@
+from asyncio import current_task
+
+from sqlalchemy.ext.asyncio import (
+    async_scoped_session,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+from core.config import settings
+
+
+class DatabaseHelper:
+    def __init__(self, url: str, echo: bool = False):
+        self.engine = create_async_engine(
+            url=url,
+            echo=echo,
+        )
+        self.session_factory = async_sessionmaker(
+            bind=self.engine,
+            autoflush=False,
+            expire_on_commit=False,
+            autocommit=False,
+        )
+        self.scoped_session = async_scoped_session(
+            session_factory=self.session_factory,
+            scopefunc=current_task,
+        )
+
+    async def session_dependency(self):
+        session = self.scoped_session()
+        try:
+            yield session
+        finally:
+            await session.close()
+            await self.scoped_session.remove()
+            
+db_helper = DatabaseHelper(url=settings.db.url, echo=settings.db.echo)
