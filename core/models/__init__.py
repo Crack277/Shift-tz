78832a1709e9a18de23347b5a1152731d0794b51
redun <redunsasha2017@gmail.com>
2026-06-08 02:03:42 +0700

    create tables

---
+++ core/models/__init__.py
@@ -0,0 +1,10 @@
+__all__ = (
+    "Base",
+    "Room",
+    "Reservation",
+    "Time_Slot",
+)
+from .base import Base
+from .reservation import Reservation
+from .room import Room
+from .time_slot import Time_Slot
--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI
 import uvicorn
 
+from contextlib import asynccontextmanager
+
 from core.api import router as api_router
-
 from core.config import settings
+from core.database_helper import db_helper
+from core.models import Base
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
+    await db_helper.engine.dispose()
 
 
-app = FastAPI()
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(api_router)
 
 
